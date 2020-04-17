fa0342ed4905fc0aa396b55f24c3b1bdf5c4cf78
Kanta Niihara <sta.niihara@gmail.com>
2020-04-17 12:08:33 +0900

    再帰下降構文解析の追加

--- 9cc.c
+++ 9cc.c
@@ -125,6 +125,80 @@ Token *tokenize(char *p) {
     return head.next;
 }
 
+
+// 抽象構文木のノードの種類
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+    NodeKind kind; // ノードの型
+    Node *lhs;	   // left hand side
+    Node *rhs;	   // right hand side
+    int val;	   // kindがND_NUMの場合の値
+};
+
+// 新しいノードを作成する関数
+// ノードが記号の場合
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+// ノードが数の場合
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr() {
+    Node *node = mul();
+
+    for (;;) {
+	if (consume('+'))
+	    node = new_node(ND_ADD, node, mul());
+	else if (consume('-'))
+	    node = new_node(ND_SUB, node, mul());
+	else
+	    return node;
+    }
+}
+
+Node *mul() {
+    Node *node = primary();
+
+    for (;;) {
+	if (consume('*'))
+	    node = new_node(ND_MUL, node, primary());
+	else if (consume('/'))
+	    node = new_node(ND_DIV, node, primary());
+	else
+	    return node;
+    }
+}
+
+Node *primary() {
+    if (consume('(')) {
+	Node *node = expr();
+        expect(')');
+	return node
+    } else {
+        return new_node_num(expect_number());
+    }
+}	    
+
 int main(int argc, char **argv){
     if (argc != 2) {
 	error("引数の個数が正しくありません");
